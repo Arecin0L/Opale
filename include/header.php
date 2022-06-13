a2ba263ede6edfb16efb2a44cc99e722d34d2394
Arecin0L <betrancourt.lonicera@gmail.com>
2022-06-13 19:08:55 +0200

    Logo
    Création du logo + insertion icon

---
+++ include/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./sass/index.css">
+    <link rel="shortcut icon" href="./img/logo_small_icon_only_inverted.png" type="image">
+    <title>Chambres d'Opale</title>
+</head>
+<body>
+    
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+include './include/header.php';
+?>
+
+<h1>Test</h1>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+include './include/footer.php';
+?>
+
